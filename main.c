437787445946adf78de9cc380090deb53fb5ab90
Robert-Gaube <robert.gabriel.gaube@intel.com>
2024-05-13 12:48:59 +0000

    VM done

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include "parser.h"
 #include "ad.h"
+#include "vm.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
@@ -11,7 +12,12 @@ int main(int argc, char *argv[]) {
     char *file_data = loadFile(argv[1]);
     Token *tokens = tokenize(file_data);
     // showTokens(tokens);
+    pushDomain();
+    vmInit();
     parse(tokens);
-    showDomain(symTable, "Global");
+    // showDomain(symTable, "Global");
+    Instr* testCode = genTestProgramDouble();
+    run(testCode);
+    dropDomain();
     return 0;
 }
